ad8defa69e6c6cb1d2ac76dc59991ca0617c6da3
Matt Miller <matt@facultycreative.com>
2014-04-29 09:11:54 -0400

    move much code to partials.

---
+++ notes.js
@@ -0,0 +1,82 @@
+You're all set! Your app should now be live. To view it run
+    cd dist && heroku open
+If you're using mongoDB, be sure to add a database to your heroku app.
+    heroku addons:add mongohq
+
+
+To deploy a new build
+    grunt build
+Then enter the dist folder to commit these updates:
+    cd dist && git commit -am "describe your changes here"
+Finally, deploy your updated build to Heroku with
+    git push heroku master
+
+/*
+    
+    // core competencies
+    openframeworks
+    javascript
+    
+    // technology
+    angularjs
+    nodejs
+    jquery
+    html5
+    api
+    geolocation
+    mongodb
+    drupal
+    php
+    tdd
+
+    // area of interest
+    presentation
+    demonstration
+    publication
+    website
+    exhibition
+    print
+    physical prototype
+    game
+    legos
+    apps
+
+    // jobs
+    !!! add tags for these genious! 
+
+    */
+
+
+    /*
+
+    FF8300  BF7930  A65500  FFA240  FFBB73
+    Secondary Color A:
+    FFB400  BF9530  A67500  FFC740  FFD673
+    Secondary Color B:
+    FF2800  BF4630  A61A00  FF5D40  FF8973
+    Complementary Color:
+    06799F  216278  024E68  3AAACF  61B4CF
+
+    openframeworks
+    javascript
+    angularjs
+    tdd
+    nodejs
+    jquery
+    html5
+    api
+    geolocation
+    mongodb
+    drupal
+    php
+
+    presentation
+    demonstration
+    publication
+    game
+    website
+    exhibition
+    print
+    legos
+    physical prototype
+    */
